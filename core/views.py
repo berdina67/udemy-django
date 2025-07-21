b04bb3b400b25a36a1a3d4bde643ecdcb29bcb47
fulvio_cloud <fulvio.murenu@pge.pr.gov.br>
2025-07-21 23:49:17 +0000

    adição de renders no arquivo views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def blog_single(request):
+    return render(request, 'blog-single.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def faq(request):
+    return render(request, 'faq.html')
